243537ecd0599b490f9122e10ec32956e13c0be0
ralracish <carlarblackman@gmail.com>
2019-03-21 18:29:51 -0400

    initial work

---
+++ controllers/burgers-controllers.js
@@ -0,0 +1,46 @@
+const express = require("express");
+
+const router = express.Router();
+
+// Import the model (burgers.js) to use its database functions.
+const cat = require("../models/burgers.js");
+
+// Create all our routes and set up logic within those routes where required.
+router.get("/", function(req,res) {
+    burgers.all(function(data) {
+        const hbsOBject = {
+            burgers: data
+        };
+        console.log(hbsObject);
+        res.render("index", hbsObject);
+    });
+});
+
+router.post("/api/burgers", function(req, res) {
+    burger.create(["burger_name", "devoured"], [req.body.burger_name, reg.body.devoured], function(result) {
+        // Send back the ID of the new burger
+        res.json({ id: result.insertID });
+    });
+});
+
+router.put("api/burgers/:id", function(req, res) {
+    const condition = "id = " + req.params.id;
+
+    console.log("condition", condition);
+
+    burger.update(
+        {devoured: req.body.devoured
+        },
+        condition,
+        function(result) {
+            if (result.changedRows === 0) {
+              // If no rows were changed, then the ID must not exist, so 404
+              return res.status(404).end();
+            }
+            res.status(200).end();
+        }
+    );
+});
+
+// Export routes for server.js to use.
+module.exports = router;
